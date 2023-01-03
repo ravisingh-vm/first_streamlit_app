360396595701c093608319d896a9caa6cd53f00a
ravisingh-vm <100830293+ravisingh-vm@users.noreply.github.com>
2023-01-03 12:01:42 +0530

    Update streamlit_app.py
    Added header and texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Memu')
+streamlit.text('Omega 3 and Blueberry Oatmeals')
+streamlit.text('Kale, spinach & Rocket Smoothie')
+streamlit.text('Hard Boiled Free-Range Eggs')
