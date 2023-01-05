461f8d82bb13299346ac5b1048fa3bfbea3feabc
ravisingh-vm <100830293+ravisingh-vm@users.noreply.github.com>
2023-01-05 16:04:51 +0530

    Update streamlit_app.py
    defining function

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,16 +22,22 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+# Create a repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+	  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)  	# streamlit.text(fruityvice_response.json()) # Just writes the data on the screen (hide the line in the app)
+		fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())               	# Take the json cersion of response and normalize it.
+		return fruityvice_normalized
+
+# New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select the food to get the information")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)  # streamlit.text(fruityvice_response.json()) # Just writes the data on the screen (hide the line in the app)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())               # Take the json cersion of response and normalize it.
-    streamlit.dataframe(fruityvice_normalized)                                              # output it the screen as a table
-  
+    back_from_function = get_fruityvice_data(fruit_choice)             
+    streamlit.dataframe(back_from_function)  
+    
 #  streamlit.write('The user entered ', fruit_choice)  ## Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
 except URLError as e:
   streamlit.error()
